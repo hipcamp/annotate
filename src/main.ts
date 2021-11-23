e1d4fd26f0f366a862a0660520d8272a3b7e0039
Caleb Gosiak <caleb@hipcamp.com>
2021-11-23 12:52:54 -0600

    v0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,46 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+// eslint-disable-next-line no-shadow
+enum ANNOTATION_LEVEL {
+  NOTICE = 'notice',
+  WARN = 'warn',
+  ERROR = 'error'
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const level: string =
+      (core.getInput('level').toLowerCase() as ANNOTATION_LEVEL) ||
+      ANNOTATION_LEVEL.NOTICE
+    const message: string = core.getInput('message')
+    const annotationProperties: core.AnnotationProperties = {
+      title: core.getInput('title'),
+      file: core.getInput('file'),
+      startLine: core.getInput('start-line')
+        ? +core.getInput('start-line')
+        : undefined,
+      endLine: core.getInput('end-line')
+        ? +core.getInput('end-line')
+        : undefined,
+      startColumn: core.getInput('start-column')
+        ? +core.getInput('start-column')
+        : undefined,
+      endColumn: core.getInput('end-column')
+        ? +core.getInput('end-column')
+        : undefined
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    switch (level) {
+      case ANNOTATION_LEVEL.NOTICE:
+        core.notice(message, annotationProperties)
+        break
+      case ANNOTATION_LEVEL.WARN:
+        core.warning(message, annotationProperties)
+        break
+      case ANNOTATION_LEVEL.ERROR:
+        core.error(message, annotationProperties)
+        break
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
